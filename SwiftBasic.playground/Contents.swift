a6163e1c07d38eab64d048bae3ce0533a6810ff2
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-02-07 21:46:24 +0900

    Type Annotations

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -1,31 +1,15 @@
 import UIKit
 
-//변수, 상수
-//상자, 그릇
+//Type Annotations
 
-var str = "Hello, playground"
+var str: String = "Hello, playground"
 
-var age = 10
+var myAge: Int = 0
 
-age = 20
-age = 30
-age = 55
+var screenHeight: Float = 560
 
-str = "hello"
-str = "hi"
+//Binary operator '+' cannot be applied to operands of type 'Int' and 'Float'
+//var sum = myAge + screenHeight
+//var sum = myAge + Int(screenHeight)
 
-//Swift 언어 특징
-//Safe type 타입 안전한, 강타입, 타입이 엄격하다.
-
-//Cannot assign value of type 'String' to type 'Int'
-//age = "hi"
-
-//Cannot assign value of type 'Int' to type 'String'
-//str = 111
-
-//상수: 값이 변하지 않음.
 let guestName = "john"
-//Cannot assign to value: 'guestName' is a 'let' constant
-//guestName = "kim"
-
-
