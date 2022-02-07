f728f55fe5e1f0f7fd4f53aaad613a1433ec0331
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-02-07 21:36:42 +0900

    var, let (변수, 상수)

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//변수, 상수
+//상자, 그릇
+
+var str = "Hello, playground"
+
+var age = 10
+
+age = 20
+age = 30
+age = 55
+
+str = "hello"
+str = "hi"
+
+//Swift 언어 특징
+//Safe type 타입 안전한, 강타입, 타입이 엄격하다.
+
+//Cannot assign value of type 'String' to type 'Int'
+//age = "hi"
+
+//Cannot assign value of type 'Int' to type 'String'
+//str = 111
+
+//상수: 값이 변하지 않음.
+let guestName = "john"
+//Cannot assign to value: 'guestName' is a 'let' constant
+//guestName = "kim"
+
+
