00c207ae66f4e0bb1aa146bd2ac0d4340739bd15
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-02-08 22:05:03 +0900

    Operators_2

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -1,34 +1,26 @@
 import UIKit
 
-//기초 연산자
-//Basic Operators
+//비교 연산자
+//Comparison Operators
 
-//+ - * / %(remainder Operator)
+let a = 20
+let b = 20
 
-let a: Int = 20
-let b: Double = 30
-//Swift언어 특징
-//타입 엄격
-//강타입
-//type safe
+//Bool
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
 
-//let c = a + b
-
-//Binary operator '/' cannot be applied to operands of type 'Int' and 'Double'
-let c = a / Int(b)
-
-let aa = "hi"
-let bb = " hello"
-let cc = aa + bb
-
-//%
-//홀수 짝수
-if b.truncatingRemainder(dividingBy: 2) == 0 {
-    print("짝수")
+if a > b {
+    
 } else {
-    print("홀수")
+    
 }
 
-var d: Int = 20
-d += 2
-d -= 2
+let c = "a"
+let d = "b"
+
+"\u{61}"
