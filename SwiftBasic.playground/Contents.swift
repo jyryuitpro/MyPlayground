15f87badf7b535fa2d6c00e4784cf4957f728708
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-02-08 21:19:07 +0900

    Operators_1

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -1,55 +1,34 @@
 import UIKit
 
-//Optionals (옵셔널)
-//값이 있을 수도 있고, 없을 수도 있다.
+//기초 연산자
+//Basic Operators
 
-//옵셔널 그냥 사용할 수 없는 경우가 많이 있음.
+//+ - * / %(remainder Operator)
 
-var a: Int? = 20
-var b: Int?
+let a: Int = 20
+let b: Double = 30
+//Swift언어 특징
+//타입 엄격
+//강타입
+//type safe
 
-//Unwrapping
-//if Statements
+//let c = a + b
 
-if a != nil {
-    print(a!)
+//Binary operator '/' cannot be applied to operands of type 'Int' and 'Double'
+let c = a / Int(b)
+
+let aa = "hi"
+let bb = " hello"
+let cc = aa + bb
+
+//%
+//홀수 짝수
+if b.truncatingRemainder(dividingBy: 2) == 0 {
+    print("짝수")
+} else {
+    print("홀수")
 }
 
-//Unwrapping + nil check
-//if let
-//if var
-if let hasNumber = a {
-    print(hasNumber)
-}
-
-if var hasNumber = a {
-    hasNumber = hasNumber * 2
-    print(hasNumber)
-}
-
-//guard let
-//guard var
-//class AAA {
-//    guard let hasNumber = a else {
-//        return
-//    }
-//}
-
-func testFunc1() {
-    guard let hasNumber = a else {
-        return
-    }
-    print(hasNumber)
-    print("end")
-}
-
-func testFunc2() {
-    guard var hasNumber = a else {
-        return
-    }
-    hasNumber = hasNumber * 2
-    print(hasNumber)
-    print("end")
-}
-
-testFunc1()
+var d: Int = 20
+d += 2
+d -= 2
