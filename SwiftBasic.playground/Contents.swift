6a911de835fca9b0ff53673117c45f494b415638
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-02-10 23:54:09 +0900

    Array 복습

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -25,7 +25,10 @@ if myNames.count > index {
 
 //myNames.append(contentsOf: ["hi", "hello"])
 myNames = myNames + ["hi", "hello"]
+
+//index
 myNames.remove(at: 0)
+
 myNames.removeFirst()
 myNames.removeLast()
 myNames.removeAll()
@@ -38,12 +41,15 @@ if myNames.isEmpty {
     
 }
 
+//index
 myNames.insert("hahaha", at: 0)
+
 
 for name in myNames {
     print(name + "님")
 }
 
+//index와 value를 같이 가져오는 방법 (Tuples)
 for (index, name) in myNames.enumerated() {
     print(index, name)
     print(name + "님")
