cfe6e0fd5263be4e0935a0f0c9319aaf86588f63
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-02-11 00:17:48 +0900

    Set

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -3,54 +3,35 @@ import UIKit
 //Collection Types
 //집합 타입
 
-//Array
-//Swift - type safe
-var myNames = Array<String>()
-var myAges = [Int]()
+//Set
+var names = Set<String>()
+names.insert("kim")
+names.insert("min")
+names.insert("lee")
+names.insert("lee")
+names
 
-myNames.append("kim")
-myNames.append("lee")
-myNames.append("jin")
+var sameNames = ["lee", "lee", "kim", "lee"]
+sameNames
+Set(sameNames)
 
-//방어코드
-//안전한 코딩 스타일
-//Fatal error: Index out of range
-//myNames[3]
+var names2: Set = ["lee", "kim", "min"]
+names2
 
-let index = 3
+var numbers1: Set = [1, 2, 3, 4, 5]
 
-if myNames.count > index {
-    myNames[index]
-}
+var numbers2: Set = [4, 5, 6, 7, 8]
 
-//myNames.append(contentsOf: ["hi", "hello"])
-myNames = myNames + ["hi", "hello"]
+var numbers3: Set = [5, 6, 7, 8]
 
-//index
-myNames.remove(at: 0)
+//교집합
+numbers1.intersection(numbers2).intersection(numbers3)
 
-myNames.removeFirst()
-myNames.removeLast()
-myNames.removeAll()
+//합집합
+numbers1.union(numbers2)
 
-if myNames.count == 0 {
-    
-}
+//합집합 - 교집합 (대칭차집합)
+numbers1.symmetricDifference(numbers2)
 
-if myNames.isEmpty {
-    
-}
-
-//index
-myNames.insert("hahaha", at: 0)
-
-
-for name in myNames {
-    print(name + "님")
-}
-
-//index와 value를 같이 가져오는 방법 (Tuples)
-for (index, name) in myNames.enumerated() {
-    print(index, name)
-    print(name + "님")
-}
+//여집합: 한쪽을 빼버리는 것
+numbers1.subtract(numbers2)
